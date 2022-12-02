10ac3b01f256e390e01dbb1451ca8faff10b0e77
Simon Beaulieu <sim.beaulieu10@gmail.com>
2022-12-02 15:47:34 -0500

    Ajout de tout ce qui est visuel

--- routes/html.js
+++ routes/html.js
@@ -30,4 +30,13 @@ router.get('/panier', function (req, res) {
   res.sendFile(path.join(__dirname, '/../client/panier.html'));
 });
 
+router.get('/payer', function (req, res) {
+  res.sendFile(path.join(__dirname, '/../client/payer.html'));
+});
+
+router.get('/confirmation', function (req, res) {
+  res.sendFile(path.join(__dirname, '/../client/confirmation.html'));
+});
+
+
 module.exports = router;
