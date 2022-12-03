e7f60464fc79505c2598272d8079c3d464a5a1ab
Leandre Postal <posl1401@usherbrooke.ca>
2022-12-02 19:44:41 -0500

    first commit

--- client/scripts/vente.js
+++ client/scripts/vente.js
@@ -1,20 +1,48 @@
 TOKEN_VENTE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MCwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjM2NzUyMzAxLCJleHAiOjE4MzY3NTk1MDF9.QYtVOl6o87doRiT2EsezLqtSpz27K-nEZ4KqcmZV5Ac";
 
 function vente_to_html(vente) {
-
+    //client = chercherClient(vente.idClient);
     vente_card = $('<div></div>').addClass('card mb4-rounded-3 shadow-sm');
     vente_head = $('<div></div>').addClass('card-header py-3').append(`<div class="row"><h4 class="my-0 fw-normal col"> Commande No.`+vente.id+`</h4>
     <label class="col status"><h5> `+vente.status+`</h5></label></div>`);
-    vente_content = $(`<div class="row">
-        <div class="col" id="list_venteProd">
+    vente_test = $(`<div class="row">
+    <div class="col" >
+        <div class="row" id="list_venteProd">
+            <h6>`+venteProd_to_html(vente.produits)+`</h6>
         </div>
-    </div>`)
-    // lmao = chargerProduits(vente);
-    chargerproduits(vente.produits);
-    vente_card.append(vente_head).append(vente_body);
+    </div>
+    <div class="col" id="list_userInfo">
+        <div class="row">
+            <h6>La vente date du : `+vente.date+`</h6>
+        </div>
+        <div class="row">
+            <div class="col" id="nique">
+            <h6> Prenom : +client.nom+ </h6>
+            <h6> </h6>
+            <h6> </h6>
+            </div>
+        </div>
+    </div>
+    </div>  `);
+    // vente_content = $(`<section>
+    //     <div class="row">
+    //             <div class="col" id="abcd">
+    //             <h6>`+vente.date+`</h6>
+    //     <div class="col" id="list_venteProd">
+
+    //     </div>
+    //     </div>
+
+
+    //     </div>
+    //     </section>`);
+    //chargerproduits(vente.produits);
+    //chercherClient(vente.idClient);
+    vente_card.append(vente_head).append(vente_test);
     return $('<div></div>').addClass('col').append(vente_card);
 }
-//            panier.items.findIndex(obj => obj.id === idItem)            <div class"row"><p class="col">`+vente.produits[0].nomProduit+`</p> <p class="col"> `+vente.produits[0].quantite+`</p></div>
+//                    <button class="btn btn-primary" onclick="chercherClient(`+vente.idClient+`)"></button>
+//    panier.items.findIndex(obj => obj.id === idItem)            <div class"row"><p class="col">`+vente.produits[0].nomProduit+`</p> <p class="col"> `+vente.produits[0].quantite+`</p></div>
 
 
 // function vente_to_html(vente){
@@ -40,15 +68,29 @@ function vente_to_html(vente) {
 //     vente_card.append(vente_head).append(vente_body);
 //     return $('<div></div>').addClass('col').append(vente_card);
 //   }
-  
-  function venteProd_to_html(produit){
+    function chercherClient(id_client){
+    $.ajax({
+        url: "/clients/" + id_client,
+        method:"GET",
+        beforeSend: function (xhr){
+            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_VENTE);
+        },
+        success: function( result ) {
+            // client = [result.nom, result.prenom, result.pays];
+            return result;
+        }});
+    }
+
+  function venteProd_to_html(produits){
+    produits.forEach(produit => {
     vente_body = $('<div></div>')
-        .append(`<div class="row">
+        .append(`<div class="">
                     <h6 class="col">`+ produit.nomProduit +` </h6>
                     <h6 class="col">`+ produit.quantite +` </h6>
                  </div>
         `)
-  
+        $('#list_venteProd').append(vente_body);
+    });
     return vente_body;
   }
   
@@ -73,6 +115,7 @@ function vente_to_html(vente) {
           success: function( result ) {
               console.log(result);
               $.each(result, function (key, value) {
+                //client = chercherClient(value.idClient);
                   vente = vente_to_html(value);
                   console.log(value);
                   $('#list_ventes').append(vente);
